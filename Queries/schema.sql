f00ab7b6163afb91cb430598dc7c6177bcd571e6
Viasha Isler <islerviasha@gmail.com>
2021-12-15 13:02:31 -0500

    Updates schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE departments(
 	UNIQUE (dept_name)
 );
 
+DROP TABLE employees;
+
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -16,41 +18,57 @@ CREATE TABLE employees(
 	PRIMARY KEY (emp_no)
 );
 
+SELECT * from employees;
+
+
+DROP TABLE managers;
 CREATE TABLE managers(
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES employees (dept_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+SELECT * FROM managers;
+
+DROP TABLE salaries;
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employess (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
+SELECT * FROM salaries;
+
+DROP TABLE department_employees;
 CREATE TABLE department_employees(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no) REFERENCES departments (dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+SELECT * FROM department_employees;
 
+DROP TABLE titles;
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (from_date)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
+SELECT * FROM titles;
 
 SELECT * from departments;
+
+SELECT * from employees;
